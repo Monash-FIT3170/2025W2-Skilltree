3ce95e688a34047cf326b1c59d1e2b94ef1a9dc2
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-04-17 14:49:35 +1000

    setup : sample schema, Mongo collection, Pub/Sub, react-meteor-data useFind & useSubscribe for reference

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Hello } from './Hello.jsx';
+import { useFind, useSubscribe } from 'meteor/react-meteor-data';
+import { SampleCollection } from '/imports/api/collections/Sample';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to SkillTree!</h1>
-    <Hello/>
-  </div>
-);
+export const App = () => { 
+  useSubscribe("sample"); // Subscribe to the "sample" publication
+  samples = useFind(() => SampleCollection.find()); // Fetch documents from SampleCollection using Meteor's useFind method for real-time updates from the database
+  return (
+    <div>
+      <h1>Welcome to SkillTree!</h1>
+      <Hello/>
+      <h3>Sample:</h3>
+      <ul>
+        { samples.map(sample => <li key={sample._id}>{sample.title} by {sample.author}: {sample.copies}x</li>) }
+      </ul>
+    </div>
+  );
+}
--- server/main.js
+++ server/main.js
@@ -1,4 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-Meteor.startup(async () => {
+import { SampleCollection } from '/imports/api/collections/Sample'; // SampleCollection (demonstration) 
 
+// Meteor Startup
+Meteor.startup(async () => {
+  // Insert sample document to SampleCollection with Schema Validation (demonstration)
+  await SampleCollection.removeAsync({}); // Drop collection content to update and avoid dupes (debug)
+  // Insert a sample document into the SampleCollection with schema validation
+  await SampleCollection.insertAsync({ title: 'Ulysses', author: 'James Joyce', copies: 2 }); // Try changing to -1 to see validation error! 
+  // Publish the publication named as "sample" from the backend, lets clients (front-end JSX) subscribe to the data in the SampleCollection for real time changes
+  Meteor.publish("sample", () => SampleCollection.find());
 });
