9003095a2b4d220c1ab2d10b9d88ccadf5537c00
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-04-24 14:45:24 +1000

    feat : added UsersCollection, Users Schema skeleton and Users Publication

---
+++ imports/api/schemas/Users.js
@@ -0,0 +1,14 @@
+import SimpleSchema from "meteor/aldeed:simple-schema";
+import { Schemas } from '/imports/api/Schemas'
+import { UsersCollection } from '/imports/api/collections/Users'; // UsersCollection
+
+// Schema References (Nested/Dependencies)
+
+
+// Define the schema for the UsersCollection using SimpleSchema to Schemas (for reusability)
+Schemas.Users = new SimpleSchema({
+
+  }); 
+
+// Attach the defined schema (from Schemas) to the UsersCollection
+UsersCollection.attachSchema(Schemas.Users);
