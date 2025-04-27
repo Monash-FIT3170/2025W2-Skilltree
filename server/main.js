404e096c9b2c55c3a28df5327ac94d3d50a5eaba
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-04-27 12:30:17 +1000

    setup : added meteor methods sample example + project structure for meteor methods

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import 'meteor/aldeed:collection2/static';
 
 import '/imports/api/Publications'; // Publications
+import '/imports/api/Methods'; // Methods
 import '/imports/Router' // Router (Server-Side Rendering)
 
 // Meteor Startup
