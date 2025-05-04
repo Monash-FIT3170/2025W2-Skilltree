e626a82696e38801a414f0ef98f928ae9b481df0
sgup0024 <sgup0024@student.monash.edu>
2025-05-04 20:12:36 +1000

    Added Proof of Practice, membership tier, expertise fields into the user area.

--- imports/api/schemas/Users.js
+++ imports/api/schemas/Users.js
@@ -1,14 +1,28 @@
 import SimpleSchema from "meteor/aldeed:simple-schema";
-import { Schemas } from '/imports/api/Schemas'
-import { UsersCollection } from '/imports/api/collections/Users'; // UsersCollection
+import { Schemas } from "/imports/api/Schemas";
+import { UsersCollection } from "/imports/api/collections/Users"; // UsersCollection
 
 // Schema References (Nested/Dependencies)
 
-
 // Define the schema for the UsersCollection using SimpleSchema to Schemas (for reusability)
 Schemas.Users = new SimpleSchema({
-
-  }); 
+  proof_of_practice_uploads: {
+    type: Array,
+    optional: true,
+    label: "Your Proof of Practice Uploads",
+  },
+  "proof_of_practice_uploads.$": Int8Array,
+  expertise_areas: {
+    type: Array,
+    optional: true,
+    label: "Users Expertise Areas",
+  },
+  "expertise_areas.$": String,
+  membership_tier: {
+    type: String,
+    label: "Membership Tier",
+  },
+});
 
 // Attach the defined schema (from Schemas) to the UsersCollection
 UsersCollection.attachSchema(Schemas.Users);
