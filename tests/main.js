daec5a89b702e4dfaf97608814d6541e7c400ad4
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-05-05 12:13:41 +1000

    setup : adjusted tests naming & structure - added --exclude-archs to test script for faster build times - main.js consolidates tests - moved tests from main.js to app.js - renamed tests/array.test.js to tests/array.js

--- tests/main.js
+++ tests/main.js
@@ -1,21 +1,3 @@
-import assert from "assert";
-import './array.test.js';
-
-describe("meteor-app", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "SkillTree");
-  });
-
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
-
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
-});
+// Tests
+import '/tests/app';
+import '/tests/array';
