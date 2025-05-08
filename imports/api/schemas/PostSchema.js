9c7eb95d5c48f26dce5e593eb9a572778576abb9
yxuu0194 <fx7165@gmail.com>
2025-05-08 12:56:40 +1000

    feat: schema for posts when inserting in database

---
+++ imports/api/schemas/PostSchema.js
@@ -0,0 +1,38 @@
+import SimpleSchema from 'meteor/aldeed:simple-schema';
+import { Schemas } from '/imports/api/Schemas';
+import { PostCollection } from '/imports/api/collections/PostCollection';
+
+Schemas.Post = new SimpleSchema({
+    title: {
+        type: String,
+        label: "Post Title",
+        min: 1,
+        max: 300,
+    },
+    description: {
+        type: String,
+        label: "Post description",
+        max: 1000,
+        optional: true,
+    },
+    verification: {
+        type: Number,
+        label: "Verification points",
+    },
+    user: {
+        type: String, // placeholder
+        label: "User who submitted post"
+    },
+    date: {
+        type: Date,
+        label: "Date post was made"
+    },
+    evidence: {
+        type: String, // placeholder
+        label: "Proof of practice"
+    }
+});
+
+PostCollection.attachSchema(Schemas.Post);
+
+export default PostCollection;
