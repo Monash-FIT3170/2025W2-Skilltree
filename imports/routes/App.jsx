057fbb4179846fcf97f326bfb6c24e3d41308c08
keziah <klan0018@student.monash.edu>
2025-05-13 21:41:49 +1000

    Changing according to review

--- imports/routes/App.jsx
+++ imports/routes/App.jsx
@@ -6,8 +6,9 @@ import { App } from '/imports/ui/App';
 // Nested/Children Routes
 import { HomeRoutes } from '/imports/routes/pages/Home';
 import { SampleRoutes } from '/imports/routes/pages/Sample';
-import { NotFoundRoutes } from '/imports/routes/pages/NotFound';
 import { PendingProofsRoutes } from '/imports/routes/pages/PendingProofs';
+import { NotFoundRoutes } from '/imports/routes/pages/NotFound';
+
 
 
 // Define Routes for App JSX layout
@@ -19,8 +20,9 @@ export const AppRoutes = [
       // Extends children array with nested routes via spread operator (...)
       ...HomeRoutes,
       ...SampleRoutes,
-      ...NotFoundRoutes, // * Last for Page not found
-      ...PendingProofsRoutes
+      ...PendingProofsRoutes,
+      ...NotFoundRoutes // * Last for Page not found
+
     ]
   }
 ];
