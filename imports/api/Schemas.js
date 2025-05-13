c42183adcd472bcdddd34f9167346c0e7bf0e852
Steven <kaingsteven123@gmail.com>
2025-05-14 00:35:04 +1000

    Adds regex for password, email and username for validation

--- imports/api/Schemas.js
+++ imports/api/Schemas.js
@@ -4,3 +4,9 @@ export const Schemas = {};
 export const Regex = {
   _id: /^([a-f\d]{24}|[\w\d]{17})$/ // Regex for Mongo _id
 };
+
+export const emailRegex = /^([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,})$/;
+
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#~^()_+=-])[A-Za-z\d@$!%*?&#~^()_+=-]{8,64}$/;
+
+export const userNameRegex = /^[a-zA-Z0-9_-]{3,20}$/;
--- imports/api/methods/ValidateUser.js
+++ imports/api/methods/ValidateUser.js
@@ -1,20 +1,49 @@
 import { Meteor } from 'meteor/meteor';
-import { Schemas } from '/imports/api/Schemas';
+import { Schemas, emailRegex, passwordRegex, userNameRegex } from '/imports/api/Schemas';
 
 Meteor.methods({
-  validateNewUser(userOptions) {
-    //Validate the userOptions against our Users Schema
-    console.log(userOptions);
-    const userSchema = Schemas.User;
+  async validateNewUser(userOptions) {
 
-    if (!userSchema) {
-      throw new Meteor.Error('schema-not-found', 'User Schema does not exist!');
+    console.log(userOptions);
+
+    //Validate the email:
+    if (!emailRegex.test(userOptions.email)){
+      throw new Meteor.Error('invalid-email', "The input email format is invalid!");
+    };
+    
+    //Check if the email has already been in use:
+    const existingEmail = await Meteor.users.findOneAsync({
+      'emails.address': userOptions.email,
+    })
+
+    if (existingEmail) {
+      throw new Meteor.Error('already-in-use-email', "This email has already been used!");
     }
 
-    //const validationContext = userSchema.newContext();
-    //const isValid = validationContext.validate(userOptions);
+    //Validate the password:
+    if (!passwordRegex.test(userOptions.password)){
+      throw new Meteor.Error('invalid-password', `Password is invalid:
+      - Minimum 8 characters
+      - Maximum 64 characters
+      - Must include uppercase, lowercase, number, and special character`);
+    }
 
-    //TODO: NOT IMPLEMENTED YET
+    //Validate the username:
+    if (!userNameRegex.test(userOptions.username)){
+      throw new Meteor.Error('invalid-username', `Username is invalid
+        -Minimum 3 characters
+        -Maximum 20 characters
+        -Can only contain lowercase, uppercase, digits, hyphens, underscores`);
+    }
+
+
+    const existingUsername = await Meteor.users.findOneAsync({
+      username: userOptions.username,
+    });
+
+    if (existingUsername) {
+      throw new Meteor.Error('username-taken', 'Username is already in use');
+    }
 
     return { success: true };
   }
