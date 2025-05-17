c3af52d76bd08214e83d40b3ca92f4dfb7832bd9
Mitch <mitch.hare03@gmail.com>
2025-05-18 00:57:17 +1000

    Implement edit comment functionality - Add editComment Meteor method that updates comments in the DB - Add edit text box and buttons for editing and submitting edits - Add validation for empty comments (including spaces only)

--- imports/ui/components/CommentSection.jsx
+++ imports/ui/components/CommentSection.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CommentsCollection } from '/imports/api/collections/Comments';
 import { useSubscribeSuspense } from 'meteor/communitypackages:react-router-ssr';
 import { useFind } from 'meteor/react-meteor-data/suspense';
+import { Meteor } from 'meteor/meteor';
 
 export const CommentSection = () => {
   // Subscribe to comments and get real-time data
@@ -10,6 +11,11 @@ export const CommentSection = () => {
     {},
     { sort: { createdAt: -1 } }
   ]);
+
+  // The id of the comment being edited. Empty string if nothing is being edited.
+  const [editingComment, setEditingComment] = useState('');
+  // The current text value of the comment being updated, continuously updated as you edit
+  const [currentText, setCurrentText] = useState('');
 
   // Format date to be more readable
   const formatDate = date => {
@@ -21,8 +27,36 @@ export const CommentSection = () => {
     });
   };
 
+  /**
+   * Initiates the edit process
+   * @param id id of the comment being edited
+   * @returns {Promise<void>}
+   */
+  const edit = async id => {
+    setEditingComment(id);
+    setCurrentText(comments.find(item => item._id === id).comment);
+  };
+
+  /**
+   * Submits a comment edit to the backend
+   * @param id id of the comment being edited
+   * @param newText the new edited text
+   * @returns {Promise<void>}
+   */
+  const submitEdit = async (id, newText) => {
+    // TODO separate this into a validate function?
+    if (newText.trim() === '') {
+      alert('Please enter a comment');
+      return;
+    }
+    await Meteor.callAsync('editComment', id, newText);
+    setEditingComment('');
+    setCurrentText('');
+  };
+
   // Shows a scrollable comment section
   return (
+    // Comment Section
     <div
       style={{
         maxHeight: '300px',
@@ -33,6 +67,7 @@ export const CommentSection = () => {
       }}
     >
       {comments.map(item => (
+        // Individual Comment
         <div
           key={item._id}
           style={{
@@ -49,11 +84,39 @@ export const CommentSection = () => {
             }}
           >
             <strong>{item.username}</strong>
+
             <span style={{ fontSize: '0.8rem', color: '#666' }}>
               {formatDate(item.createdAt)}
             </span>
           </div>
-          <p style={{ margin: '5px 0' }}>{item.comment}</p>
+          {/* If this comment is being edited, show an edit box and submit button, else show the comment and an edit button */}
+          {editingComment === item._id ? (
+            <div>
+              <textarea
+                onChange={e => setCurrentText(e.target.value)}
+                value={currentText}
+                className="w-full bg-white"
+              ></textarea>
+              <button
+                className="text-center border-2 border-emerald-950 bg-emerald-600 text-white font-bold py-1 px-2 rounded hover:bg-emerald-700 active:bg-emerald-500 mt-2"
+                onClick={() => {
+                  submitEdit(item._id, currentText);
+                }}
+              >
+                Save
+              </button>
+            </div>
+          ) : (
+            <div>
+              <p style={{ margin: '5px 0' }}>{item.comment}</p>
+              <button
+                className="text-center border-2 border-gray-950 bg-gray-600 text-white font-bold py-1 px-2 rounded hover:bg-gray-700 active:bg-gray-500 mt-2"
+                onClick={() => edit(item._id)}
+              >
+                Edit
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
