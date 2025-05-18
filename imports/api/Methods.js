fbb3ab67595edefc90982981b04a1e95ba111c12
Romal Patel <romalpatel@gmail.com>
2025-05-19 01:02:39 +1000

    add imports

--- imports/api/Methods.js
+++ imports/api/Methods.js
@@ -2,3 +2,5 @@
 import '/imports/api/methods/Sample'; // Load Sample Collection Methods (demonstration)
 import '/imports/api/methods/Proof';
 import '/imports/api/methods/Upload';
+import '/imports/api/methods/skillTreeMethods';
+
