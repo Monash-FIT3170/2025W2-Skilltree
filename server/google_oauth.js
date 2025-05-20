452d3cc11835cd4940302d6ddc3a7fa10e136f7f
Steven <kaingsteven123@gmail.com>
2025-05-21 01:14:31 +1000

    refactor: security update

--- server/google_oauth.js
+++ server/google_oauth.js
@@ -1,8 +1,8 @@
 import { ServiceConfiguration } from 'meteor/service-configuration';
 import { Meteor } from 'meteor/meteor';
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const CLIENT_ID = Meteor.settings.private.google.clientId;
+const CLIENT_SECRET = Meteor.settings.private.google.secret;
 
 Meteor.startup(() => {
   ServiceConfiguration.configurations.upsertAsync(
