8600bb3410ad6d3c608efcc6b7c44e0e1940f248
Mitch <mitch.hare03@gmail.com>
2025-05-21 22:15:06 +1000

    Remove leftover broken schema code

--- imports/api/schemas/PostSchema.js
+++ imports/api/schemas/PostSchema.js
@@ -30,14 +30,6 @@ Schemas.Post = new SimpleSchema({
   date: {
     type: Date,
     label: 'Date post was made'
-  },
-  'proof.$': {
-    type: Schemas.Proof, // placeholder
-    label: 'Proof of practice'
-  },
-  'comments.$': {
-    type: Schemas.Comments, // comments schema
-    label: 'Comments'
   }
 });
 
