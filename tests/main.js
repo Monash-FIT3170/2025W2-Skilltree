fdbdf200fa32a627778135d93e829d3b72819a27
Romal Patel <romalpatel@gmail.com>
2025-05-22 12:32:27 +1000

    delete test (need to fix something and recreate)

--- tests/main.js
+++ tests/main.js
@@ -2,7 +2,7 @@
 // import '/tests/app';
 // import '/tests/array';
 // import '/tests/proofUploadForm';
-import '/tests/regexSearchSkillTree';
+// import '/tests/regexSearchSkillTree';
 import '/tests/app';
 import '/tests/array';
 import '/tests/proofUploadForm';
