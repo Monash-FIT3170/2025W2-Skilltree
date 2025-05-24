f8aa4fc8c65944a4c713c4b353de7110da1f8078
Mitch <mitch.hare03@gmail.com>
2025-05-24 22:26:18 +1000

    Increase background shadow

--- imports/ui/components/ProofUploadForm.jsx
+++ imports/ui/components/ProofUploadForm.jsx
@@ -223,7 +223,7 @@ export const ProofUploadForm = () => {
       <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
       {openModal && (
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/40"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
           onClick={() => setOpenModal(false)}
         >
           <div
