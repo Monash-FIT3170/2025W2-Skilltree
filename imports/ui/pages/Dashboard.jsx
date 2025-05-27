f99de1f2442720e3a865e4d7af960e50a634fd9d
Steven <kaingsteven123@gmail.com>
2025-05-27 20:53:01 +1000

    Revert "feat: Implement user drop down with items Profile, Settings and Sign Out button"
    This reverts commit f3e17a608967a138f520a59e671a421e6b8fc3be.

--- imports/ui/pages/Dashboard.jsx
+++ imports/ui/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
+import { Meteor } from 'meteor/meteor';
 import React, { Suspense, useContext } from 'react';
 import { Helmet } from 'react-helmet';
+import { Button } from 'flowbite-react';
 
 import { Fallback } from '/imports/ui/components/Fallback';
 import { UserContext } from '/imports/utils/contexts/UserContext';
@@ -17,6 +19,10 @@ export const Dashboard = () => {
         <h1 className="text-3xl font-bold mt-2">
           Welcome @{username} to SkillTree!
         </h1>
+
+        <Button color="green" size="sm" onClick={() => Meteor.logout()} pill>
+          logout
+        </Button>
 
         <h3 className="font-sans text-[30px] font-bold text-left">
           MY SKILLTREES
