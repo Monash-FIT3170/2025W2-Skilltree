c1108acd632daaec7c5d008edd9ec080f9eee1fc
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-05-27 21:46:13 +1000

    feat : removed ProfileDashboard (moved to Dashboard)

--- imports/routes/App.jsx
+++ imports/routes/App.jsx
@@ -13,7 +13,6 @@ import { ProofUploadRoutes } from '/imports/routes/pages/ProofUpload';
 import { ProofDetailsRoutes } from '/imports/routes/pages/ProofDetails';
 import { SkillTreeCommunityRoutes } from '/imports/routes/pages/SkillTreeCommunity';
 import { CreateSkillTreeRoutes } from '/imports/routes/pages/CreateSkillTree';
-import { ProfileDashboardRoutes } from '/imports/routes/pages/ProfileDashboard';
 
 // Define Routes for App JSX layout
 export const AppRoutes = [
@@ -34,7 +33,6 @@ export const AppRoutes = [
       ...ProofDetailsRoutes,
       ...SkillTreeCommunityRoutes,
       ...CreateSkillTreeRoutes,
-      ...ProfileDashboardRoutes,
       ...NotFoundRoutes // * Last for Page not found
     ]
   }
--- imports/routes/pages/ProfileDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Element JSX Component/Layout
-import { ProfileDashboard } from '/imports/ui/pages/ProfileDashboard';
-
-export const ProfileDashboardRoutes = [
-  { path: 'profiledashboard', element: <ProfileDashboard /> }
-];
