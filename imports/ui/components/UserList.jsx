2f1583f14ec6d4d11df80ea9639d2358886d4466
yxuu0194 <fx7165@gmail.com>
2025-05-28 09:51:31 +1000

    feat - component displaying list of users in skill tree

---
+++ imports/ui/components/UserList.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+
+export const UserList = ({skillTreeId}) => {
+    // call meteor method skilltrees.get
+    const getSkillTree = async () => {
+        const skillTree = Meteor.call('skilltrees.get',skillTreeId);
+        return skillTree.subscribers;
+    }
+
+    // call meteor method user 
+    const getUserInfo = async userId => {
+        const user = Meteor.user.findOneAsync({_id: userId});
+        return user;
+    }
+
+    // array of userId
+    const subscribers = getSkillTree();
+
+    // NOT FORMATTED
+    return (
+        <div class = "relative overflow-x-auto">
+            <table>
+                <thead>
+                    <tr>
+                        User ID
+                    </tr>
+                    <tr>
+                        Username
+                    </tr>
+                    <tr>
+                        User Email
+                    </tr>
+                </thead>
+                <tbody>
+                    {subscribers.map(userId => (
+                        <tr>
+                            <td>{userId}</td>
+                            <td>{getUserInfo(userId.username)}</td>
+                            <td>{getUserInfo(userId.emails[0])}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
