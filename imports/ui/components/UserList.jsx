97f3dc5c36c57434361d5a2ef60b0b203c25cfb0
yxuu0194 <fx7165@gmail.com>
2025-05-28 09:52:07 +1000

    fix - ran lint fix

--- imports/ui/components/UserList.jsx
+++ imports/ui/components/UserList.jsx
@@ -1,47 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
 
-export const UserList = ({skillTreeId}) => {
-    // call meteor method skilltrees.get
-    const getSkillTree = async () => {
-        const skillTree = Meteor.call('skilltrees.get',skillTreeId);
-        return skillTree.subscribers;
-    }
+export const UserList = ({ skillTreeId }) => {
+  // call meteor method skilltrees.get
+  const getSkillTree = async () => {
+    const skillTree = Meteor.call('skilltrees.get', skillTreeId);
+    return skillTree.subscribers;
+  };
 
-    // call meteor method user 
-    const getUserInfo = async userId => {
-        const user = Meteor.user.findOneAsync({_id: userId});
-        return user;
-    }
+  // call meteor method user
+  const getUserInfo = async userId => {
+    const user = Meteor.user.findOneAsync({ _id: userId });
+    return user;
+  };
 
-    // array of userId
-    const subscribers = getSkillTree();
+  // array of userId
+  const subscribers = getSkillTree();
 
-    // NOT FORMATTED
-    return (
-        <div class = "relative overflow-x-auto">
-            <table>
-                <thead>
-                    <tr>
-                        User ID
-                    </tr>
-                    <tr>
-                        Username
-                    </tr>
-                    <tr>
-                        User Email
-                    </tr>
-                </thead>
-                <tbody>
-                    {subscribers.map(userId => (
-                        <tr>
-                            <td>{userId}</td>
-                            <td>{getUserInfo(userId.username)}</td>
-                            <td>{getUserInfo(userId.emails[0])}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  // NOT FORMATTED
+  return (
+    <div class="relative overflow-x-auto">
+      <table>
+        <thead>
+          <tr>User ID</tr>
+          <tr>Username</tr>
+          <tr>User Email</tr>
+        </thead>
+        <tbody>
+          {subscribers.map(userId => (
+            <tr>
+              <td>{userId}</td>
+              <td>{getUserInfo(userId.username)}</td>
+              <td>{getUserInfo(userId.emails[0])}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
