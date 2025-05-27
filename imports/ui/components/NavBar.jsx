fbab64d67fa2389fa34e0017bce3a2c598df8f4e
Aaron See <104244862+rev46@users.noreply.github.com>
2025-05-27 22:24:10 +1000

    remove sample and 404 pages from nav bar

--- imports/ui/components/NavBar.jsx
+++ imports/ui/components/NavBar.jsx
@@ -134,18 +134,6 @@ export const NavBar = () => {
           Home
         </Link>
         <Link
-          to="/Sample"
-          className="text-white hover:bg-gray-600 px-3 py-2 rounded"
-        >
-          Sample
-        </Link>
-        <Link
-          to="/404"
-          className="text-white hover:bg-gray-600 px-3 py-2 rounded"
-        >
-          404
-        </Link>
-        <Link
           to="/pendingproofs"
           className="text-white hover:bg-gray-600 px-3 py-2 rounded"
         >
