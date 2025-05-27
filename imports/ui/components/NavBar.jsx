df3b0b1175ecc67e9b5898ee30994c2f26367483
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-05-27 21:24:55 +1000

    fix : lint for Navbar

--- imports/ui/components/NavBar.jsx
+++ imports/ui/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 import {
