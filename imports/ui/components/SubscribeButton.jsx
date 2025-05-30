dcb8b6e98dae6723c1ac48e376ccce2528220a91
yxuu0194 <fx7165@gmail.com>
2025-05-30 21:47:04 +1000

    feat - added pointer when hovering over subscribe/unsubscribe and user list buttons

--- imports/ui/components/SubscribeButton.jsx
+++ imports/ui/components/SubscribeButton.jsx
@@ -110,7 +110,7 @@ export const SubscribeButton = ({ skillTreeId }) => {
   if (isLoading) {
     return (
       <div className="flex flex-wrap items-start gap-2 w-15/100">
-        <Button className="w-full position-relative mt-2 text-white text-2xl font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0">
+        <Button className="cursor-pointer w-full position-relative mt-2 text-white text-2xl font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0">
           <Spinner
             size="sm"
             aria-label="Info spinner example"
@@ -130,7 +130,7 @@ export const SubscribeButton = ({ skillTreeId }) => {
           pill
           type="submit"
           onClick={unsubscribeUserFromSkilltree}
-          className="w-full position-relative mt-2 text-white text-2xl font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
+          className="cursor-pointer w-full position-relative mt-2 text-white text-2xl font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
         >
           Unsubscribe
         </Button>
@@ -140,7 +140,7 @@ export const SubscribeButton = ({ skillTreeId }) => {
           pill
           type="submit"
           onClick={subscribeUserToSkilltree}
-          className="w-full position-relative mt-2 text-white text-2xl font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
+          className="cursor-pointer w-full position-relative mt-2 text-white text-2xl font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
         >
           Subscribe
         </Button>
