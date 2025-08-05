31c6b9e41d10ea825d29acb2a17999be20d07f74
Steven <kaingsteven123@gmail.com>
2025-08-06 01:10:24 +1000

    feat: Adds settings jsx component with account info, password and subscription tab

---
+++ imports/routes/pages/Configuration/Settings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Settings } from '../../../ui/pages/Configuration/Settings';
+
+export const SettingRoutes = [
+  {
+    path: '/settings',
+    element: <Settings />
+  }
+];
