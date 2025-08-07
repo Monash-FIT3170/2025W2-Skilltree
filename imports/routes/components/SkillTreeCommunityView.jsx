bca89c5c745c66dd6f97e5fd7003ef69a5712d81
yxuu0194 <fx7165@gmail.com>
2025-08-08 02:48:45 +1000

    feat - added CommunityLeaderboardView as child rout of SkillTreeCommunityView

--- imports/routes/components/SkillTreeCommunityView.jsx
+++ imports/routes/components/SkillTreeCommunityView.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 // Element JSX Component/Layout
 import { SkillTreeCommunityView } from '/imports/ui/components/SkillTreeCommunityView';
 
+import { CommunityLeaderboardRoutes } from '/imports/routes/pages/CommunityLeaderboard';
+
 export const SkillTreeCommunityViewRoutes = [
   {
     path: ':id',
-    element: <SkillTreeCommunityView />
+    element: <SkillTreeCommunityView />,
+    children: [
+      ...CommunityLeaderboardRoutes
+    ]
   }
 ];
