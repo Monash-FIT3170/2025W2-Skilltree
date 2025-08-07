69fcb11cd87e17bc6f559070630312d65322b855
Marcus Chow <mcchow008@gmail.com>
2025-08-07 23:19:17 +1000

    Add Schema, Methods, Publications, Collection for storing user skilltree progresses

--- imports/api/collections/SkillTreeProgress.js
+++ imports/api/collections/SkillTreeProgress.js
@@ -1,5 +1,3 @@
 import { Mongo } from 'meteor/mongo';
 
-export const SkillTreeProgressCollection = new Mongo.Collection(
-  'skillTreeProgress'
-);
+export const SkillTreeProgressCollection = new Mongo.Collection('skillTreeProgress');
