2940dc13f6c334052a96d77ff1aaf5023fea829c
yxuu0194 <fx7165@gmail.com>
2025-08-08 02:48:10 +1000

    feat - route for leaderboard page

---
+++ imports/routes/pages/CommunityLeaderboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Element JSX Component/Layout
+import { CommunityLeaderboard } from '/imports/ui/pages/CommunityLeaderboard.jsx';
+
+export const CommunityLeaderboardRoutes = [
+  { path: 'leaderboard', element: <CommunityLeaderboard /> }
+];
