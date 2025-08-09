2724a88c74bc689f1d91bfec78b03677c22b2ce9
Steven <kaingsteven123@gmail.com>
2025-08-09 16:44:36 +1000

    update: reverted auth provider changes

--- imports/utils/providers/AuthProvider.jsx
+++ imports/utils/providers/AuthProvider.jsx
@@ -7,17 +7,6 @@ import { AuthContext } from '/imports/utils/contexts/AuthContext';
 
 // Create Provider
 export const AuthProvider = ({ children }) => {
-  const { userId, completedProfile } = useTracker(() => {
-    return {
-      userId: Meteor.userId(),
-      completedProfile: false
-    };
-  });
-
-  const value = {
-    userId,
-    completedProfile
-  };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  const userId = useTracker(() => Meteor.userId());
+  return <AuthContext.Provider value={userId}>{children}</AuthContext.Provider>;
 };
