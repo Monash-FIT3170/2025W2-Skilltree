012e62792094a448d14c14bd0ee96ac7ef019767
Steven <kaingsteven123@gmail.com>
2025-08-11 15:38:35 +1000

    bug: switch case error

--- imports/ui/components/Dashboard/Greeting.jsx
+++ imports/ui/components/Dashboard/Greeting.jsx
@@ -2,28 +2,29 @@ import React from 'react';
 
 export const getGreetingMessage = () => {
   const currHour = new Date().getHours();
-  switch (currHour) {
-    case currHour >= 5 && currHour < 12:
-      return 'Good morning';
-    case currHour >= 12 && currHour < 17:
-      return 'Good afternoon';
-    case currHour >= 17 && currHour < 21:
-      return 'Good evening';
-    default:
-      return 'Good night';
+  console.log(currHour);
+
+  if (currHour >= 5 && currHour < 12) {
+    return 'Good morning';
+  } else if (currHour >= 12 && currHour < 17) {
+    return 'Good afternoon';
+  } else if (currHour >= 17 && currHour < 21) {
+    return 'Good evening';
+  } else {
+    return 'Good night';
   }
 };
 
 export const getGreetingIcon = () => {
   const currHour = new Date().getHours();
-  switch (currHour) {
-    case currHour >= 5 && currHour < 12:
-      return '🌅';
-    case currHour >= 12 && currHour < 17:
-      return '☀️';
-    case currHour >= 17 && currHour < 21:
-      return '🌆';
-    default:
-      return '🌙';
+
+  if (currHour >= 5 && currHour < 12) {
+    return '🌅';
+  } else if (currHour >= 12 && currHour < 17) {
+    return '☀️';
+  } else if (currHour >= 17 && currHour < 21) {
+    return '🌆';
+  } else {
+    return '🌙';
   }
 };
