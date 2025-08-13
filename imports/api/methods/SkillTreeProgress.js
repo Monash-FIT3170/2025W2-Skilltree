50042322fd8ace5f406cc4d6fbacdae9014c56c2
Marcus Chow <mcchow008@gmail.com>
2025-08-13 23:34:52 +1000

    Add method for retrieving a stored skill tree progress

--- imports/api/methods/SkillTreeProgress.js
+++ imports/api/methods/SkillTreeProgress.js
@@ -3,30 +3,65 @@ import { SkillTreeProgressCollection } from '/imports/api/collections/SkillTreeP
 import { check } from 'meteor/check';
 
 Meteor.methods({
-    async saveSkillTreeProgress(skillTreeId, progressTree) {
-        check(skillTreeId, String);
-        check(progressTree, [Object]);
-
-        const existing = await SkillTreeProgressCollection.findOneAsync({
-            userId: this.userId,
-            skillTreeId
-        });
-
-        if (existing) {
-            return await SkillTreeProgressCollection.updateAsync(
-                { userId: this.userId, skillTreeId },
-                {
-                    $set: {
-                        progress: progressTree,
-                    }
-                }
-            );
-        }
-        else {
-            return await SkillTreeProgressCollection.insertAsync({
-                userId: this.userId,
-                skillTreeId,
-                progress: progressArray,
-            });
-        }
+  async getSkillTreeProgress(skillTreeId) {
+    check(skillTreeId, String);
+    const existing = await SkillTreeProgressCollection.findOneAsync({
+      userId: this.userId,
+      skillTreeId
     });
+
+    if (existing) {
+      return existing;
+    } else {
+      return null;
+    }
+  },
+
+  async saveSkillTreeProgress(
+    skillTreeId,
+    progressTreeNodes,
+    progressTreeEdges
+  ) {
+    check(skillTreeId, String);
+    check(progressTreeNodes, [Object]);
+    check(progressTreeEdges, [Object]);
+
+    const existing = await SkillTreeProgressCollection.findOneAsync({
+      userId: this.userId,
+      skillTreeId
+    });
+
+    if (existing) {
+      return await SkillTreeProgressCollection.updateAsync(
+        { userId: this.userId, communityId: skillTreeId },
+        {
+          $set: {
+            skillNodes: progressTreeNodes,
+            skillEdges: progressTreeEdges
+          }
+        }
+      );
+    } else {
+      return await SkillTreeProgressCollection.insertAsync({
+        userId: this.userId,
+        skillTreeId,
+        skillNodes: progressTreeNodes,
+        skillEdges: progressTreeEdges
+      });
+    }
+  },
+
+  async removeSkillTreeProgress(skillTreeId) {
+    check(skillTreeId, String);
+    const existing = await SkillTreeProgressCollection.findOneAsync({
+      userId: this.userId,
+      skillTreeId
+    });
+
+    if (existing) {
+      return SkillTreeProgressCollection.remove(skillTreeId);
+    } else {
+      return null;
+    }
+  }
+});
