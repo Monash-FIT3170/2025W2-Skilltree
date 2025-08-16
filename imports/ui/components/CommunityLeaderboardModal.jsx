6dfa7b7fe4a73c619dcb893c34ca0f9066433b7c
yxuu0194 <fx7165@gmail.com>
2025-08-17 02:24:28 +1000

    feat - increase modal size - increased size of leaderboard modal

--- imports/ui/components/CommunityLeaderboardModal.jsx
+++ imports/ui/components/CommunityLeaderboardModal.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React , {Suspense} from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'flowbite-react';
 import { CommunityLeaderboardList } from '/imports/ui/components/CommunityLeaderboardList';
+import { Fallback } from '/imports/ui/components/Fallback';
 
 export const CommunityLeaderboardModal = () => {
-    console.log('hello world')
-
     // id from url
     const { id } = useParams();
 
@@ -16,14 +16,28 @@ export const CommunityLeaderboardModal = () => {
     }
 
   return (
-    <Modal show={true} onClose={closeModal} dismissible>
-    <ModalHeader>Community Leaderboard</ModalHeader>
-      <ModalBody>
-        <CommunityLeaderboardList skillTreeId = {id}></CommunityLeaderboardList>
-      </ModalBody>
-      <ModalFooter>
-        <Button onClick={closeModal}>Close</Button>
-      </ModalFooter>
-    </Modal>
+    <div>
+      <Modal 
+        show={true} 
+        onClose={closeModal} 
+        dismissible
+        size="7xl"
+      >
+        <ModalHeader>Community Leaderboard</ModalHeader>
+        <ModalBody>
+            <CommunityLeaderboardList skillTreeId = {id}></CommunityLeaderboardList>
+        </ModalBody>
+        <ModalFooter>
+          <Button 
+            color = "green"
+            onClick={closeModal}
+            pill
+            className="cursor-pointer position-relative text-lg font-bold mt-2 text-white font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
+            >
+              Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
   )
 }
