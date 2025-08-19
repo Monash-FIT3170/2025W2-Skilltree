e27f0577e1de81fa292233e232e62dd054e44ce3
Laetitia Teo <laetitiat8888@gmail.com>
2025-08-19 11:29:10 +1000

    running lint:fix

--- imports/ui/pages/CreateSkillForest.jsx
+++ imports/ui/pages/CreateSkillForest.jsx
@@ -1,7 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import { Meteor } from 'meteor/meteor';
-
 // JSX UI
 import { CreateForestForm } from '/imports/ui/components/SkillForestComponents/CreateForestForm';
 
