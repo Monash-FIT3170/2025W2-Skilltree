2608a5802e8ed74cc570c47f107794fb78dcab72
Mitch <mitch.hare03@gmail.com>
2025-08-19 19:58:10 +1000

    Add documentation for filter parameter

--- imports/ui/components/CommunityLeaderboardModal.jsx
+++ imports/ui/components/CommunityLeaderboardModal.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import {
@@ -84,7 +84,7 @@ export const CommunityLeaderboardModal = () => {
           color="green"
           onClick={closeModal}
           pill
-          className="cursor-pointer position-relative text-lg font-bold mt-2 text-white font-semibold leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
+          className="cursor-pointer position-relative text-lg font-bold mt-2 text-white leading-none !font-sans flex items-center gap-3 px-6 py-3 bg-[#328E6E] rounded-[22px] transition-all duration-200 hover:bg-[#2a7a5e] focus:outline-none focus:ring-0"
         >
           Close
         </Button>
