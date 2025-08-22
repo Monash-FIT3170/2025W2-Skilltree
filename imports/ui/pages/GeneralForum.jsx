9c55beb0024322d27867d9b69d48e38a83169eeb
jtan0276 <jtan0276@student.monash.edu>
2025-08-22 12:08:03 +1000

    hover back to topic colour change

--- imports/ui/pages/GeneralForum.jsx
+++ imports/ui/pages/GeneralForum.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { TopicList } from '/imports/ui/components/TopicListGeneralForum';
+import { Tooltip } from 'flowbite-react';
 
 export const GeneralForum = () => {
   const { skilltreeId } = useParams();
@@ -149,10 +150,10 @@ export const GeneralForum = () => {
           </form>
 
           <button
-            className="bg-gray-300 text-black px-4 py-2"
+            className="bg-gray-300 text-black px-4 py-2 hover:bg-gray-400"
             onClick={() => setSelectedTopicId(null)}
-          >
-            Back to Topics
+          > 
+            Back to Topics 
           </button>
         </>
       )}
