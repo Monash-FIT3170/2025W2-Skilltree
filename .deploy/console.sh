da51ed59f0add8affe78e565180109f46f30f798
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-08-23 12:03:05 +1000

    feat: updated bash script (refactor session names), added deployment info in README and updated setup script

--- .deploy/console.sh
+++ .deploy/console.sh
@@ -6,5 +6,5 @@ echo "DO NOT PRESS CTRL+C OR CTRL+D TO EXIT"
 echo "Ctrl+A then D to detach from the session instead."
 read -t 5 -p "Press Enter to continue..." input # Wait 5 sec or until enter is pressed
 
-# Enter screen session named skilltree or create & enter if it does not exist
-screen -r skilltree || screen -S skilltree && screen -r skilltree
+# Enter screen session named webserver or create & enter if it does not exist
+screen -x webserver || screen -S webserver
