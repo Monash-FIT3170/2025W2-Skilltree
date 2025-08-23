e4556b12faf9e4911729abc2d07e49e2a9c150c4
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-08-23 10:20:45 +1000

    feat: Add bash script for Ubuntu server setup

---
+++ .deploy/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Linux Server Setup: Ubuntu 24.04 LTS (Noble) amd64
+#
+# git clone https://github.com/Monash-FIT3170/2025W2-Skilltree
+# chmod +x ./2025W2-Skilltree/.deploy/setup.sh
+# ./2025W2-Skilltree/.deploy/setup.sh
+
+PROJECT_DIR="$HOME/2025W2-Skilltree"
+
+# Update Ubuntu
+sudo apt update && sudo apt upgrade -y
+
+# Install packages
+sudo apt install screen micro btop gdu 
+
+# NodeJS (24) Installation
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24
+
+# MeteorJS Installation
+npx meteor
+export PATH=/home/ubuntu/.meteor:$PATH
+
+# Project Setup
+cd $PROJECT_DIR
+meteor npm run setup
+
+# Allow unprivileged port 80
+sudo echo 'net.ipv4.ip_unprivileged_port_start=0' > /etc/sysctl.d/50-unprivileged-ports.conf
+sudo sysctl --system
