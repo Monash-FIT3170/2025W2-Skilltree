908dace74824c27f8e98095ebd33c8b041ea09fe
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-08-28 12:11:49 +1000

    feat: Add <SuspenseHydrated /> used to opt out of SSR for components with subbed data that may change on page refresh to workaround full SSR subbed mismatch issues

---
+++ imports/utils/SuspenseHydrated.jsx
@@ -0,0 +1,30 @@
+import { Meteor } from 'meteor/meteor';
+import React, { Suspense, useEffect, useState } from 'react';
+import { FastRender } from 'meteor/communitypackages:fast-render';
+
+// JSX UI
+import { Fallback } from '/imports/ui/components/SiteFrame/Fallback';
+
+export const SuspenseHydrated = ({
+  children, // <SuspenseData> <The child JSX /> </SuspenseData>
+  fallback = <Fallback /> //  <The Fallback JSX />
+}) => {
+  const [isHydrated, setIsHydrated] = useState(false); // Track if client is hydrated yet
+
+  useEffect(() => {
+    FastRender.onDataReady(() => setIsHydrated(true)); // onDataReady = hydrated, safe to render subscription calls
+  }, []);
+
+  return (
+    <Suspense fallback={fallback}>
+      {
+        (Meteor.settings.public.enablePartialSRR != false && // PartialSRR AND...
+          Meteor.isServer && // Server side AND...
+          !Meteor.settings.public.enableSSR) || // Not Full SSR OR...
+        (!isHydrated && !Meteor.settings.public.enableSSR) // PartialSRR waiting for hydration in client
+          ? fallback // For partialSSR only, server will always render fallback or client waiting for hydration
+          : children // Always show in fullSSR/noSSR or only after client hydrates for partialSSR to workaround 'hydration mismatch' :)
+      }
+    </Suspense>
+  );
+};
