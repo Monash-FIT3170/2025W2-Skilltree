49038c3318a56410b95fe89830c453c0b82f9b1f
Romal Patel <romalpatel@gmail.com>
2025-08-30 18:56:31 +1000

    update SkillTree.insert method

--- imports/api/methods/SkillTree.js
+++ imports/api/methods/SkillTree.js
@@ -1,10 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import { SkillTreeCollection } from '/imports/api/collections/SkillTree';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 // import { Schemas } from '/imports/api/schemas/SkillTree';
+
+const AWS_REGION = 'ap-southeast-2';
+const AWS_BUCKET = '2025w2-skilltree';
+
+const AWSAccessKeyId = Meteor.settings.private.AWSAccessKeyId;
+const AWSSecretAccessKey = Meteor.settings.private.AWSSecretKey;
+
+const s3 = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWSAccessKeyId,
+    secretAccessKey: AWSSecretAccessKey
+  }
+});
 
 // Basic methods for SkillTreeCollection
 Meteor.methods({
-  'skilltrees.insert'(skilltree) {
+  async 'skilltrees.insert'(skilltree) {
     // Schemas.SkillTree.validate(skilltree); // TO DO: fix validate errors, seems to be issue with Schema handling and importing
     const {
       title,
@@ -16,12 +31,31 @@ Meteor.methods({
       skillEdges = []
     } = skilltree || {};
 
+    let imageUrl = image;
+
+    // Upload base64 image to S3
+    if (image && image.startsWith('data:')) {
+      const buffer = Buffer.from(image.split(',')[1], 'base64');
+      const fileName = `skilltrees/${Date.now()}-${Math.random().toString(36).substring(7)}.png`;
+
+      const command = new PutObjectCommand({
+        Bucket: AWS_BUCKET,
+        Key: fileName,
+        Body: buffer,
+        ContentType: 'image/png'
+      });
+
+      await s3.send(command);
+
+      imageUrl = `https://${AWS_BUCKET}.s3.${AWS_REGION}.amazonaws.com/${fileName}`;
+    }
+
     const doc = {
       title,
       description,
       termsAndConditions,
       tags,
-      image,
+      image: imageUrl,
       skillNodes,
       skillEdges,
       owner: this.userId,
@@ -31,6 +65,7 @@ Meteor.methods({
 
     return SkillTreeCollection.insertAsync(doc);
   },
+
 
   async 'skilltrees.insertAsync'(skillTree) {
     return await SkillTreeCollection.insertAsync(skillTree);
