5e4e8e1de007e874ba6277a82c115e4927faf2ec
BENTIA-1 <ben.tia.582@gmail.com>
2025-09-03 10:48:21 +1000

    clean up

--- imports/ui/components/Dashboard/EmptyState.jsx
+++ imports/ui/components/Dashboard/EmptyState.jsx
@@ -6,7 +6,7 @@ export const EmptyState = () => {
   const navigate = useNavigate();
 
   const handleEmptyStateClick = () => {
-    navigate('/search-communities');
+    navigate('/SearchResults');
   };
   return (
     <div className="col-span-full flex flex-col items-center justify-center py-12 px-4">
