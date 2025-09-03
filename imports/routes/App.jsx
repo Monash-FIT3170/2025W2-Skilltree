753e974e30dee7c62e25b114f81e36ae448ab14a
BENTIA-1 <ben.tia.582@gmail.com>
2025-09-03 10:48:21 +1000

    clean up

--- imports/routes/App.jsx
+++ imports/routes/App.jsx
@@ -16,7 +16,6 @@ import { CreateSkillTreeRoutes } from '/imports/routes/pages/CreateSkillTree';
 import { GeneralForumRoutes } from '/imports/routes/pages/GeneralForum';
 import { SettingRoutes } from '/imports/routes/pages/Configuration/Settings';
 import { SearchResultsRoutes } from '/imports/routes/pages/SearchResults';
-import { GeneralForumRoutes } from '/imports/routes/pages/GeneralForum';
 
 // Define Routes for App JSX layout
 export const AppRoutes = [
