bf44b2a365f976d7db0b9d8bf0446090ee7d21f6
Ankush <aank0004@student.monash.edu>
2025-09-04 18:37:08 +1000

    refactor: remove SkillTreeProgress API methods, publications, and schemas

--- imports/api/collections/SkillTreeProgress.js
+++
@@ -1,7 +0,0 @@
-// import { Mongo } from 'meteor/mongo';
-
-// export const SkillTreeProgressCollection = new Mongo.Collection(
-//   'skillTreeProgress'
-// );
-
-// Meteor.publish('skillTreeProgress', () => SkillTreeProgressCollection.find());
