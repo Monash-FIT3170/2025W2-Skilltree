00f3045d4e7ade2892a373e1eb20b0c8c4828b9c
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-09-04 11:15:32 +1000

    feat: Opt out of SSR for Dashboard Greeting (timezone mismatch) and DashboardSkillTrees (sort mismatch)

--- imports/ui/pages/Dashboard.jsx
+++ imports/ui/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, Suspense } from 'react';
 import { FiUsers } from '@react-icons/all-files/fi/FiUsers';
 import { User } from '/imports/utils/User';
+import { SuspenseHydrated } from '../../utils/SuspenseHydrated';
 
 // JSX UI
 import { DashboardSkillTrees } from '/imports/ui/layouts/DashboardSkillTrees';
 import { DashboardLoadingState } from '../components/Dashboard/LoadingState';
+import { GreetingLoadingState } from '../components/Dashboard/GreetingLoadingState';
 import {
   getGreetingIcon,
   getGreetingMessage
@@ -30,12 +32,15 @@ export const Dashboard = () => {
       <div className="p-4 lg:p-6 max-w-7xl mx-auto">
         {/* Intro message */}
         <div className="mb-8 bg-gradient-to-r text-[#328E6E] rounded-xl p-6 border-l-4 border-[#328E6E] shadow-lg">
-          <div className="flex items-center gap-3 mb-2">
-            <span className="text-3xl">{greetingIcon}</span>
-            <h1 className="text-2xl lg:text-4xl font-bold text-[#328E6E]">
-              {greeting}, {user?.profile?.givenName}!
-            </h1>
-          </div>
+          {/* Opt out of SSR due to datetime mismatching on server and client hydration */}
+          <SuspenseHydrated fallback={<GreetingLoadingState />}>
+            <div className="flex items-center gap-3 mb-2">
+              <span className="text-3xl">{greetingIcon}</span>
+              <h1 className="text-2xl lg:text-4xl font-bold text-[#328E6E]">
+                {greeting}, {user?.profile?.givenName}!
+              </h1>
+            </div>
+          </SuspenseHydrated>
           <p className="text-gray-600 ml-12">
             Ready to continue your learning journey?
           </p>
@@ -57,9 +62,10 @@ export const Dashboard = () => {
             <FiUsers size={20} className="text-[#04BF8A]" />
             My Skill Trees
           </h2>
-          <Suspense fallback={<DashboardLoadingState />}>
+          {/* Opt out of SSR due to processing (sort) useFind data causing mismatch on hydration */}
+          <SuspenseHydrated fallback={<DashboardLoadingState />}>
             <DashboardSkillTrees key={user._id} />
-          </Suspense>
+          </SuspenseHydrated>
         </div>
       </div>
     </div>
