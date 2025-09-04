3f1c9293d8fd2683d3cdf6853c29fd4f94edfda1
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-09-04 11:06:42 +1000

    feat: add back <SuspenseHydrated /> to allow opting out of SSR selectively for full SSR - adjust default fallback message

--- imports/ui/components/SiteFrame/Fallback.jsx
+++ imports/ui/components/SiteFrame/Fallback.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 
-export const Fallback = ({
-  msg = `Loading... ${Meteor.settings.public.enableSSR ? '(refresh page if stuck)' : ''}`
-}) => <div className="fallback fadeInEffect">{msg}</div>;
+export const Fallback = ({ msg = 'Loading...' }) => (
+  <div className="fallback fadeInEffect">{msg}</div>
+);
