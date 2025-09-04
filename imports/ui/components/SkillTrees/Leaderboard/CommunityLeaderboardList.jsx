a9cc1e8a04ea5bcac7d34fc981a98002534b3c75
yxuu0194 <fx7165@gmail.com>
2025-09-04 18:23:31 +1000

    fix: used conditional styling and ref assignment for jsx

--- imports/ui/components/SkillTrees/Leaderboard/CommunityLeaderboardList.jsx
+++ imports/ui/components/SkillTrees/Leaderboard/CommunityLeaderboardList.jsx
@@ -143,39 +143,12 @@ export const CommunityLeaderboardList = ({ skillTreeId, filter }) => {
   return (
     <List unstyled className="divide-y divide-gray-200 relative space-y-0">
       {leaderboard.map((entry, index) => {
-        if (entry._id === currUserId) {
+        const isCurrentUser = entry._id === currUserId;
           return (
             <ListItem
               key={entry._id}
-              ref={userRef}
-              className="bg-green-100 py-3"
-              onClick={() => console.log(entry)}
-            >
-              <div className="flex items-center">
-                <div className="flex w-2/20 items-center justify-center-safe">
-                  <Badge
-                    color="green"
-                    size="sm"
-                    className="rounded-full p-1.5 w-[4ch] tabular-nums items-center justify-center inline-flex hover:bg-green-100"
-                    clearTheme={{ hover: true }}
-                  >
-                    {String(index + 1)}
-                  </Badge>
-                </div>
-                <div className="flex w-3/20 items-center justify-center-safe">
-                  {`${entry.username}`}
-                </div>
-                <div className="flex w-6/20 items-center justify-center-safe">
-                  {filter === 'totalXp' ? entry.totalXp : entry.numComments}
-                </div>
-              </div>
-            </ListItem>
-          );
-        } else {
-          return (
-            <ListItem
-              key={entry._id}
-              className="py-3"
+              ref={isCurrentUser ? userRef : null}
+              className={`py-3 ${isCurrentUser ? "bg-green-100" : ""}`}
               onClick={() => console.log(entry)}
             >
               <div className="flex items-center">
@@ -197,7 +170,6 @@ export const CommunityLeaderboardList = ({ skillTreeId, filter }) => {
               </div>
             </ListItem>
           );
-        }
       })}
 
       {isInList && (
