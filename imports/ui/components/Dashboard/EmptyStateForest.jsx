ad7fe305ddec9fcfbb8b96a88d89939016b57667
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-09-05 18:58:37 +1000

    fix: refactor class -> className, stroke-linecap -> strokeLinecap, stroke-width -> strokeWidth

--- imports/ui/components/Dashboard/EmptyStateForest.jsx
+++ imports/ui/components/Dashboard/EmptyStateForest.jsx
@@ -11,7 +11,7 @@ export const EmptyStateForest = () => {
     <div className="col-span-full flex flex-col items-center justify-center py-12 px-4">
       <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center mb-4">
         <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
+          className="w-6 h-6 text-gray-800 dark:text-white"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -21,8 +21,8 @@ export const EmptyStateForest = () => {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeWidth="2"
             d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
           />
         </svg>
--- imports/ui/components/SkillForest/SubscribedTreesPopup.jsx
+++ imports/ui/components/SkillForest/SubscribedTreesPopup.jsx
@@ -57,7 +57,7 @@ export const SubscribedTreesPopup = ({ skillTreeId }) => {
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-1 text-gray-500">
             <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
+              className="w-6 h-6 text-gray-800 dark:text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
