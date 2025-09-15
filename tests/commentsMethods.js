43a2720cfb983454ce0f338581e6f51ae6a10793
Mitch <mitch.hare03@gmail.com>
2025-09-15 19:40:56 +1000

    Attempt to fix comment test failing by inserting dummy proofs and skilltrees

--- tests/commentsMethods.js
+++ tests/commentsMethods.js
@@ -2,12 +2,183 @@ import assert from 'assert';
 import { Meteor } from 'meteor/meteor';
 import '/imports/api/methods/Comments';
 import { CommentsCollection } from '/imports/api/collections/Comments'; // Comment collection
+import { SkillTreeCollection } from '/imports/api/collections/SkillTree';
+import { ProofCollection } from '/imports/api/collections/Proof';
+
+const userId = 'testUserId';
+
+const skillTreeId1 = 'dfaZsdfasdfdsf';
+const skillTree1 = {
+  _id: skillTreeId1,
+  title: 'Basketball',
+  image:
+    'https://media.istockphoto.com/id/1636022764/photo/basketball-ball.jpg?s=612x612&w=0&k=20&c=NVi1V5dCAZKUHdrhnRq-G5t8XSvZE1YXvgw8NxX3N0I=',
+  description: 'Learn dribbling to shooting.',
+  termsAndConditions:
+    'This SkillTree is intended for sports training purposes.',
+  tags: ['basketball', 'sports', 'ball'],
+  skillNodes: [
+    {
+      id: '0',
+      type: 'root',
+      data: {
+        label: 'root',
+        description: 'root',
+        progressXp: null,
+        requirements: 'root',
+        xpPoints: null
+      },
+      position: { x: 0, y: 0 }
+    },
+    {
+      id: '1',
+      type: 'view-node-unlocked',
+      data: {
+        label: 'basic dribbling 🏀',
+        description: 'Learn how to dribble the basketball effectively.',
+        progressXp: 10,
+        requirements: 'Upload a video of yourself dribbling for 10 seconds',
+        xpPoints: 10
+      },
+      position: { x: 200, y: 300 }
+    },
+    {
+      id: '2',
+      type: 'view-node-unlocked',
+      data: {
+        label: 'Layup 🏃‍♂️',
+        description:
+          ' A close-range shot taken by driving toward the basket and laying the ball off the backboard.',
+        progressXp: 6,
+        requirements: 'Upload a video of yourself',
+        xpPoints: 10
+      },
+      position: { x: 200, y: 200 }
+    },
+    {
+      id: '3',
+      type: 'view-node-locked',
+      data: {
+        label: 'Spin Move 😵',
+        description: 'Learn how to do a spin move.',
+        progressXp: 0,
+        requirements: 'Upload a video of yourself',
+        xpPoints: 20
+      },
+      position: { x: 200, y: 100 }
+    },
+    {
+      id: '4',
+      type: 'view-node-unlocked',
+      data: {
+        label: 'Agility 💨',
+        description: 'Learn how to be agile.',
+        progressXp: 34,
+        requirements:
+          'Upload a video of yourself doing the illinois agility test',
+        xpPoints: 50
+      },
+      position: { x: 0, y: 100 }
+    },
+    {
+      id: '5',
+      type: 'view-node-unlocked',
+      data: {
+        label: 'Shooting Form 🎯',
+        description: 'Learn how to do proper shooting form.',
+        progressXp: 20,
+        requirements: 'Upload a video of yourself',
+        xpPoints: 20
+      },
+      position: { x: -200, y: 300 }
+    },
+    {
+      id: '6',
+      type: 'view-node-unlocked',
+      data: {
+        label: 'Free Throws 💸',
+        description: 'Learn how to do free throws.',
+        progressXp: 6,
+        requirements: 'Upload a video of yourself',
+        xpPoints: 20
+      },
+      position: { x: -150, y: 200 }
+    },
+    {
+      id: '7',
+      type: 'view-node-locked',
+      data: {
+        label: 'Three Pointers 💧',
+        description: 'Learn how to do a spin move.',
+        progressXp: 0,
+        requirements: 'Upload a video of yourself',
+        xpPoints: 20
+      },
+      position: { x: -250, y: 100 }
+    },
+    {
+      id: '8',
+      type: 'view-node-unlocked',
+      data: {
+        label: 'Mid Range 🥶',
+        description: 'Learn how to do a spin move.',
+        progressXp: 4,
+        requirements: 'Upload a video of yourself',
+        xpPoints: 20
+      },
+      position: { x: -350, y: 200 }
+    }
+  ],
+  skillEdges: [
+    { id: 'e1', source: '0', target: '7' },
+    { id: 'e2', source: '0', target: '4' },
+    { id: 'e3', source: '0', target: '3' },
+    { id: 'e4', source: '3', target: '2' },
+    { id: 'e5', source: '2', target: '1' },
+    { id: 'e6', source: '7', target: '6' },
+    { id: 'e7', source: '7', target: '8' },
+    { id: 'e8', source: '6', target: '5' },
+    { id: 'e9', source: '8', target: '5' }
+  ],
+  admins: ['basketballpro'],
+  subscribers: [
+    'playerA',
+    'playerB',
+    userId,
+    'User 1',
+    'User 2',
+    'User 3',
+    'User 4'
+  ]
+};
 
 // _id
-const COMMENT_ID_1 = 'abcd12345';
-const TEST_ID_1 = 'a1b2c3d4';
-const TEST_ID_2 = 'a2b3c4d5';
 
+const TEST_ID_1 = 'a1Zb2c3d4';
+const TEST_ID_2 = 'a2Zb3c4d5';
+
+// THESE BEING PICKED UP BY PROOF TESTS
+const TEST_PROOF_1 = {
+  _id: TEST_ID_1,
+  title: 'Test Proof 1',
+  description: 'Desc',
+  verification: 1,
+  user: userId,
+  skillTreeId: skillTreeId1,
+  date: new Date()
+};
+
+const TEST_PROOF_2 = {
+  _id: TEST_ID_2,
+  title: 'Test Proof 2',
+  description: 'Desc',
+  verification: 1,
+  user: userId,
+  skillTreeId: skillTreeId1,
+  date: new Date()
+};
+
+const COMMENT_ID_1 = 'abcd12345';
 const NEW_TEXT = 'This comment is edited';
 
 // dummy data
@@ -40,6 +211,8 @@ const COMMENT_4 = {
   createdAt: new Date()
 };
 
+// TODO i believe dummy proofs have skilltrees that dont exist, hence subscription check fails
+
 const CommentAdd = async comment => {
   const res = await Meteor.call('addComment', comment);
   return res;
@@ -68,6 +241,11 @@ const CommentEdit = commentId => async newText => {
 before(async function () {
   // drop collection
   await CommentsCollection.removeAsync({});
+  await SkillTreeCollection.removeAsync({});
+  await ProofCollection.removeAsync({});
+  await SkillTreeCollection.insertAsync(skillTree1);
+  await ProofCollection.insertAsync(TEST_PROOF_1);
+  await ProofCollection.insertAsync(TEST_PROOF_2);
 });
 
 describe('Comment methods', function () {
@@ -107,3 +285,5 @@ describe('Comment methods', function () {
     });
   });
 });
+
+
