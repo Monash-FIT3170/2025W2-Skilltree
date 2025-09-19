8bb4e3042fe5bab8f63ebd57f495f4186ad3cb61
jtan0276 <jtan0276@student.monash.edu>
2025-09-19 10:41:02 +1000

    debug the navigation from other screens to leaderboard bug

--- imports/ui/components/SkillTrees/NavigationMenu.jsx
+++ imports/ui/components/SkillTrees/NavigationMenu.jsx
@@ -64,14 +64,11 @@ export const NavigationMenu = ({ id }) => {
   };
 
   const getLinkClasses = link => {
-    const isActive =
-      location.pathname === link ||
-      (link === `/skilltree/${id}` && location.pathname === `/skilltree/${id}`); // Default active for Community Tree
-
-    return `flex items-center gap-2 block py-2 px-3 rounded transition-all duration-200 ${
-      isActive ? 'bg-gray-600 text-white' : 'text-white hover:bg-gray-600'
-    }`;
-  };
+  const isActive = location.pathname === link || location.pathname.endsWith(link);
+  return `flex items-center gap-2 block py-2 px-3 rounded transition-all duration-200 ${
+    isActive ? 'bg-gray-600 text-white' : 'text-white hover:bg-gray-600'
+  }`;
+};
 
   const menuItems = [
     userId !== skilltree?.owner && {
@@ -95,17 +92,18 @@ export const NavigationMenu = ({ id }) => {
       )
     },
     {
-      id: 'leaderboard',
-      element: (
-        <Link
-          to={`leaderboard`}
-          state={{ background: location }}
-          className={getLinkClasses(`/skilltree/${id}/leaderboard`)}
-        >
-          <span>Leaderboard</span>
-        </Link>
-      )
-    },
+  id: 'leaderboard',
+  element: (
+    <Link
+      to={`/skilltree/${id}/leaderboard`}
+      state={{ background: location }}
+      className={getLinkClasses(`/skilltree/${id}/leaderboard`)}
+    >
+      <span>Leaderboard</span>
+    </Link>
+  )
+},
+
     isUserSubscribed && {
       id: 'help-community',
       element: (
