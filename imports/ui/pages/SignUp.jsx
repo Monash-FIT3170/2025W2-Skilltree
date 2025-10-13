a89dd4735b6bd38739a02b216f918eb697983136
Steven Kaing <kaingsteven123@gmail.com>
2025-10-13 15:10:17 +1100

    fix: removed props

--- imports/ui/pages/SignUp.jsx
+++ imports/ui/pages/SignUp.jsx
@@ -3,10 +3,5 @@ import React from 'react';
 import { AccountSetup } from '/imports/ui/layouts/Auth/AccountSetup';
 
 export const SignUp = () => (
-  <AccountSetup
-    showStepBar={true}
-    currentStep={1}
-    totalSteps={3}
-    showFooter={true}
-  />
+  <AccountSetup/>
 );
