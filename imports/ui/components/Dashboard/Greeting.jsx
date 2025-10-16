1da0b4a35597b02b97b2529ad1b240670ebb08fc
Jason Sakolkraisorn <jsakdev@mailbox.org>
2025-10-16 20:41:26 +1100

    fix: refactored Greeting to use inline Script to work with SSR

--- imports/ui/components/Dashboard/Greeting.jsx
+++ imports/ui/components/Dashboard/Greeting.jsx
@@ -1,10 +1,12 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import serialize from 'serialize-javascript';
 
 const getGreeting = type => {
-  const isIcon = type === 'icon';
-  const currHour = new Date().getHours();
-
   const greeting = () => {
+    const isIcon = type === 'icon';
+    const currHour = new Date().getHours();
+
     if (currHour >= 5 && currHour < 12) {
       return isIcon ? '🌅' : 'Good morning';
     } else if (currHour >= 12 && currHour < 17) {
@@ -16,7 +18,29 @@ const getGreeting = type => {
     }
   };
 
-  return greeting();
+  // Define client side inline script as literal string to inject before hydration, sanitised via serialize-javascript
+  const inlineScript = `
+    (function() {
+      const script = document.currentScript; // Reference to this <script> element on the DOM
+      
+      // Set value before <script> using the client's browser time rather than the server's
+      script && (script.before(
+        (${serialize(greeting).replace('()=>', 'function() ').replace('type', serialize(type))})()
+      ));
+      
+      // Remove itself <script> to clean up DOM and avoid hydration mismatch
+      script && script.remove();
+    })();
+  `;
+
+  return (
+    <>
+      {Meteor.isClient && greeting()}
+      {Meteor.isServer && (
+        <script dangerouslySetInnerHTML={{ __html: inlineScript }} />
+      )}
+    </>
+  );
 };
 
 export const getGreetingMessage = () => getGreeting('message');
--- imports/ui/pages/Dashboard.jsx
+++ imports/ui/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import React, { Suspense, useState, useEffect } from 'react';
 import { User } from '/imports/utils/User';
 import { ToastContainer, Flip } from 'react-toastify';
@@ -67,7 +68,9 @@ export const Dashboard = () => {
       <div className="p-4 lg:p-6 max-w-7xl mx-auto">
         {/* Intro message */}
         <div className="mb-8 bg-gradient-to-r text-[#328E6E] rounded-xl p-6 border-l-4 border-[#328E6E] shadow-lg">
-          <div className="flex items-center gap-3 mb-2 popInEffect">
+          <div
+            className={`flex items-center gap-3 mb-2 ${Meteor.isClient && 'popInEffect'}`}
+          >
             <span className="text-3xl">{greetingIcon}</span>
             <h1 className="text-2xl lg:text-4xl font-bold text-[#328E6E]">
               {greeting}, {user?.profile?.givenName}!
